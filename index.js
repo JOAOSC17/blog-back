5d78c51bd91cc63dcd75f630f6cf47f1ebb52070
João Costa <costav1402@gmail.com>
2021-11-14 12:03:38 -0300

    connect mongo

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(console.log("Connnected to Mongo")).catch((err)=>console.log(err));
 app.listen("5000",()=>{
     console.log("Backend is running.");
 });
